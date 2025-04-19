9a3a2ae749c3f66cb17deac6acf36532ded9544e
Jimmy <Jimmy@marscompany.co>
2025-04-19 19:41:01 +0900

    no message

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "505382942"
+    serviceWorkerVersion: "283801409"
   }
 });
